eaf6d7c47c760cfc80d18568a043b9bdd8fab4cc
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-04-27 16:04:56 +0100

    dice layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React from 'react';
 import './App.css';
+import Die from './components/Die';
 
 function App() {
+
+  function randomNum() {
+    return Math.floor(Math.random * 6)
+
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <main className="App">
+      <div className="dice-container">
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+        <Die value={randomNum}/>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/Die.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+export default function Die(props) {
+    return(
+
+        <div className='die'>
+            <h3>2{props.value}</h3>
+        </div>
+    )}
