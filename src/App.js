e6a8885e6337e5aa6788d8faddc0f3338b4237f7
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-04-27 16:58:07 +0100

    get random numbers function

--- src/App.js
+++ src/App.js
@@ -4,24 +4,24 @@ import Die from './components/Die';
 
 function App() {
 
-  function randomNum() {
-    return Math.floor(Math.random * 6)
-
+  const randomNum = () => {
+    return Math.floor(Math.random() * 6)
   }
 
   return (
     <main className="App">
       <div className="dice-container">
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
-        <Die value={randomNum}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        <Die value={randomNum()}/>
+        
       </div>
     </main>
   );
