11a55f9aa0ba0298151f53411aac6e1e7711c431
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-04-27 20:00:00 +0100

    dice array added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Die from './components/Die';
 
+
 function App() {
 
-  const randomNum = () => {
-    return Math.floor(Math.random() * 6)
+  const [dice, setdice] = useState(allDiceNums())
+
+  // gets an array of random numbers 
+  function allDiceNums() {
+    let allDice = []
+    for(let i = 0; i < 10; i++){
+      allDice.push(Math.ceil(Math.random() * 6))
+    }
+    return allDice
+
   }
+
+// maps over the dice numbers array creating a Die component for each index 
+  const diceElements = dice.map((num) => <Die value={num}/>)
+  
+  
+  console.log(dice)
+
 
   return (
     <main className="App">
       <div className="dice-container">
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        <Die value={randomNum()}/>
-        
+        {diceElements}
       </div>
     </main>
   );
