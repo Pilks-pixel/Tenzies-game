82085403d5b8c3ef294075fe98521355edcae1d8
Pilks-pixel <79462252+Pilks-pixel@users.noreply.github.com>
2022-04-28 14:31:04 +0100

    button and conditional styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Die from './components/Die';
+import { nanoid } from 'nanoid'
 
 
 function App() {
 
   const [dice, setdice] = useState(allDiceNums())
 
+  // give dice a state of held when clicked
+  // do not change state if held
+  // change die color if state == held
+
   // gets an array of random numbers 
   function allDiceNums() {
     let allDice = []
-    for(let i = 0; i < 10; i++){
-      allDice.push(Math.ceil(Math.random() * 6))
+    for (let i = 0; i < 10; i++) {
+      allDice.push({ 
+        value: Math.ceil(Math.random() * 6), 
+        isHeld: true,
+        dieId: nanoid()
+       })
+
     }
     return allDice
 
   }
 
-// maps over the dice numbers array creating a Die component for each index 
-  const diceElements = dice.map((num) => <Die value={num}/>)
-  
-  
+  // maps over the dice array creating a Die component for each index 
+  // const diceElements = dice.map((die) => <Die key={die.dieId} value={die.value} held={die.isHeld}/>)
+  const diceElements = dice.map((die) => <Die key={die.dieId} value={die} />)
+
   console.log(dice)
+
+  // triggers a new dice values
+  function handleClick() {
+    setdice(allDiceNums())
+  }
 
 
   return (
@@ -29,6 +44,7 @@ function App() {
       <div className="dice-container">
         {diceElements}
       </div>
+      <button onClick={handleClick} >Roll</button>
     </main>
   );
 }
--- src/components/Die.js
+++ src/components/Die.js
@@ -3,9 +3,11 @@ import React from "react";
 
 
 export default function Die(props) {
+    const styles = props.value.isHeld?  {backgroundColor: '#59E391'} : {backgroundColor: '#FFFFFF'}
+
     return(
 
-        <div className='die'>
-            <h3>{props.value}</h3>
+        <div className='die' style={styles} >
+            <h3>{props.value.value}</h3>
         </div>
     )}
